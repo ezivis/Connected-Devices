85e9b2a221f674a8da271303d48693b1fe39e8e4
ezivis <49287721+ezivis@users.noreply.github.com>
2022-05-14 21:42:29 -0400

    Update server.js

--- final/server.js
+++ final/server.js
@@ -1,13 +1,3 @@
-/*
-  Websocket server with express.js and express-ws.js
-  (https://www.npmjs.com/package/express-ws)
-  Serves an index page from /public. That page makes
-  a websocket client back to this server.
-
-  created 17 Jan 2021
-  by Tom Igoe
-*/
-
 var express = require('express');			    // include express.js
 // a local instance of express:
 var server = express();
@@ -15,10 +5,11 @@ var server = express();
 var wsServer = require('express-ws')(server);
 // list of client connections:
 var clients = new Array;
-
+let knownClients1 = ["::ffff:192.168.3.113"];
+let knownClients2 = ["::ffff:192.168.3.114"];
+let knownClients3 = ["::ffff:192.168.3.115"];
 // serve static files from /public:
 server.use('/', express.static('public'));
-
 // this runs after the server successfully starts:
 function serverStart() {
   var port = this.address().port;
@@ -28,25 +19,38 @@ function serverStart() {
 function handleClient(thisClient, request) {
   console.log("New Connection");        // you have a new client
   clients.push(thisClient);    // add this client to the clients array
+  
 
   function endClient() {
     // when a client closes its connection
     // get the client's position in the array
     // and delete it from the array:
     var position = clients.indexOf(thisClient);
-    clients.splice(position, 1);
+    clients.splice(position,0); 
     console.log("connection closed");
   }
 
+  thisClient.on('close', endClient);
+
   // if a client sends a message, print it out:
   function clientResponse(data) {
-    console.log(request.connection.remoteAddress + ': ' + data);
-    broadcast(data);
+    console.log(request.connection.remoteAddress + ' : ' + data);
+    if (knownClients1.includes(request.connection.remoteAddress)) {
+      console.log("Hi I'm light snesor");
+      broadcast(data);
+    }
+    if (knownClients2.includes(request.connection.remoteAddress)) {
+      console.log("Hello,I'm 2");
+      broadcast(+data + 2048);
+    }
+    if (knownClients3.includes(request.connection.remoteAddress)) {
+      console.log("Hello,I'm door");
+      broadcast(+data + 10001);
+    }
   }
 
   // set up client event listeners:
   thisClient.on('message', clientResponse);
-  thisClient.on('close', endClient);
 }
 
 // This function broadcasts messages to all webSocket clients
